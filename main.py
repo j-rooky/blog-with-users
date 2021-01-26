16d9fed99c6d2eb93eda56ff1c480c3169088cbc
Jamie Rookyard <jamie.rookyard@gmail.com>
2021-01-26 11:32:15 +0000

    Add .gitignore File.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from flask_gravatar import Gravatar
 from functools import wraps
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = "SECRET_KEY"
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
