c44ca45b2a3eb51cb7c763fb76f1879c380111cc
Jamie Rookyard <jamie.rookyard@gmail.com>
2021-01-26 12:09:08 +0000

    Add sqlite as alternative for local dev. Change debug to False.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from functools import wraps
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "sqlite:///blog.db")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -216,4 +216,4 @@ def delete_post(post_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
